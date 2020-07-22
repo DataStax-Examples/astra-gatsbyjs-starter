46694f34026385693ce6e802c8f3f5395b77f17e
Alex Leventer <alexleventer@gmail.com>
2020-07-21 20:06:56 -0700

    remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,19 +26,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
     {
       resolve: `gatsby-source-graphql`,
       options: {
